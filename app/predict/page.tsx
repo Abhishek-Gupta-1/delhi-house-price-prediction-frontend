b9238357984069cdf76879b9e0ed2e323da9a592
Abhishek Gupta <abhisheknew2023@gmail.com>
2024-03-17 16:01:44 +0530

    some changes

--- app/predict/page.tsx
+++ app/predict/page.tsx
@@ -220,7 +220,7 @@ const Predict = () => {
 
                             <div className='justify-center pt-16'>
                                 <button className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-8 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>
-                                    <Link href="/submit">Submit</Link>
+                                    <Link href="/submit">Predict Submit</Link>
                                 </button>
                             </div>
 
